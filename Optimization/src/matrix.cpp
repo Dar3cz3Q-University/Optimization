c981eb265383d90d3fa3484362b44031a30ecaf2
Dariusz Homa <45432804+Dar3cz3Q@users.noreply.github.com>
2024-10-29 18:24:38 +0100

    Add docs (#8)
    * Added license
    
    * Added license to all files
    
    * Updated premake-cmake
    
    * Added first task
    
    * Folder name

--- Optimization/src/matrix.cpp
+++ Optimization/src/matrix.cpp
@@ -1,3 +1,13 @@
+/*********************************************
+Kod stanowi uzupe�nienie materia��w do �wicze�
+w ramach przedmiotu metody optymalizacji.
+Kod udost�pniony na licencji CC BY-SA 3.0
+Autor: dr in�. �ukasz Sztangret
+Katedra Informatyki Stosowanej i Modelowania
+Akademia G�rniczo-Hutnicza
+Data ostatniej modyfikacji: 19.09.2023
+*********************************************/
+
 // Ten plik nie powinien by� edytowany
 
 #include "matrix.h"
