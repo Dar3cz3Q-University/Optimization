2edd18b8d953038f95cb73f47d1de714c2a87d27
Dariusz Homa <dariuszhoma7@gmail.com>
2024-11-04 21:24:35 +0100

    Added simple test

--- Optimization - Tests/tests/lab 2/opt_alg_test.cpp
+++ Optimization - Tests/tests/lab 2/opt_alg_test.cpp
@@ -19,7 +19,7 @@ TEST(HJ, handles_defined_values)
 	double s = 0.1;
 	double alpha = 0.1;
 	double epsilon = 1e-10;
-	double Nmax = 10000;
+	int Nmax = 10000;
 
 	// When
 	solution result = HJ(lab2_fun, x, s, alpha, epsilon, Nmax);
@@ -37,6 +37,26 @@ TEST(HJ_trial, handles_defined_values)
 
 TEST(Rosen, handles_defined_values)
 {
-	// TODO: Write some tests ;)
+	// Given
+	matrix x(2, 1);
+	x(0) = 0.5;
+	x(1) = 0.75;
+
+	matrix s(2, 1);
+	s(0) = 0.1;
+	s(1) = 0.1;
+
+	double alpha = 1.1;
+	double beta = 0.50;
+	double epsilon = 1e-10;
+	int Nmax = 10000;
+
+	// When
+	solution result = Rosen(lab2_fun, x, s, alpha, beta, epsilon, Nmax);
+
+	// Then
+	EXPECT_NEAR(result.x(0), 0.7747, EPSILON);
+	EXPECT_NEAR(result.x(1), 0.7747, EPSILON);
+	EXPECT_NEAR(m2d(result.y), 1.23967, EPSILON);
 }
 } // namespace lab_2
--- Optimization/src/main.cpp
+++ Optimization/src/main.cpp
@@ -167,7 +167,7 @@ void lab2()
 	double alpha_1 = 0.1;
 	double beta = 0.50;
 	double epsilon = 1e-4;
-	double Nmax = 100;
+	int Nmax = 1000;
 
 	/*for (int i = 0; i < 100; i++)
 	{
@@ -183,8 +183,6 @@ void lab2()
 		solution::clear_calls();
 	}*/
 
-	cout << "ROSEN" << endl << endl;
-
 	matrix x2(2, 1);
 	x2(0) = 0.5;
 	x2(1) = 0.75;
@@ -199,35 +197,9 @@ void lab2()
 	cout << result2 << endl;
 	solution::clear_calls();
 
-void lab2() 
-{
-	matrix test(2, 1);
-	test(0) = 1.0;
-	test(1) = 1.0;
-
-	std::cout << lab2_fun(test) << "\n";
-
-	double s = 0.1;
-	double alpha = 0.1;
-	double epsilon = 1e-10;
-	double Nmax = 10000;
-
-	for (int i = 0; i < 100; i++)
-	{
-		matrix x(2, 1);
-		x(0) = RandomNumberGenerator::Get().Double(-1.0, 1.0);
-		x(1) = RandomNumberGenerator::Get().Double(-1.0, 1.0);
-
-		solution result = HJ(lab2_fun, x, s, alpha, epsilon, Nmax);
-		if (abs(m2d(result.y)) < 0.01)
-			std::cout << result << "\n";
-		solution::clear_calls();
-	}
-}
 	solution result3 = HJ(lab2_fun, x2, s, alpha_1, epsilon, Nmax);
 	cout << result3 << endl;
 	solution::clear_calls();
-
 }
 
 void lab3() {}
