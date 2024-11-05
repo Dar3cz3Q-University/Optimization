ae5e1fc4c72fed4f54d57f913e4c0580099eda08
Dariusz Homa <dariuszhoma7@gmail.com>
2024-11-05 11:22:20 +0100

    Hotfix: cmake lib

--- Scripts/Setup-Linux.sh
+++ Scripts/Setup-Linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x Vendor/Binaries/Premake/Linux/premake5
-Vendor/Binaries/Premake/Linux/premake5 --cc=clang --file=premake5.lua gmake2
+Vendor/Binaries/Premake/Linux/premake5 --cc=clang --scripts=Vendor/premake-cmake gmake2
