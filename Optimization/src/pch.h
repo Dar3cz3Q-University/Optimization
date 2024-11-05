77bd3d83165aed4cb8ba3c38877c77cf2156fa82
Dariusz Homa <dariuszhoma7@gmail.com>
2024-11-05 14:00:13 +0100

    Check if it works on linux

--- Optimization/src/pch.h
+++ Optimization/src/pch.h
@@ -9,5 +9,13 @@
 #include <filesystem>
 #include <ctime>
 
-#define _USE_MATH_DEFINES
-#include <math.h>
+#ifdef _WIN32
+    #define _USE_MATH_DEFINES
+    #include <math.h>
+#else
+    #include <cmath>
+#endif
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
