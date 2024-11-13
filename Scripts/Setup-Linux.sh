7c7e1da90cb87957e474888d62ce4f0f454293af
Dariusz Homa <dariuszhoma7@gmail.com>
2024-11-13 18:45:23 +0100

    Remove calls from premake

--- Scripts/Setup-Linux.sh
+++ Scripts/Setup-Linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 chmod +x Vendor/Binaries/Premake/Linux/premake5
-Vendor/Binaries/Premake/Linux/premake5 --cc=clang --scripts=Vendor/premake-cmake gmake2
+Vendor/Binaries/Premake/Linux/premake5 --cc=clang gmake2
