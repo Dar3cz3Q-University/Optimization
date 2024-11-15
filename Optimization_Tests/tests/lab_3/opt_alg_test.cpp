03f07b492e9c9425e0770a061fa4b0a32b8902dc
Dariusz Homa <dariuszhoma7@gmail.com>
2024-11-15 21:19:09 +0100

    Added test files

---
+++ Optimization_Tests/tests/lab_3/opt_alg_test.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "gtest/gtest.h"
+
+namespace lab_3
+{
+
+TEST(pen, handles_defined_values)
+{
+	// TODO: Write some tests ;)
+}
+
+TEST(sym_NM, handles_defined_values)
+{
+	// TODO: Write some tests ;)
+}
+
+} // namespace lab_3
---
+++ Optimization_Tests/tests/lab_3/user_funs_test.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "gtest/gtest.h"
+
+#include "optimization/user_funs.h"
+#include "optimization/solution.h"
+
+namespace lab_3
+{
+
+TEST(lab3_fun_1, handles_defined_values)
+{
+	// TODO: Write some tests ;)
+}
+
+TEST(lab3_fun_2, handles_defined_values)
+{
+	// TODO: Write some tests ;)
+}
+
+} // namespace lab_3
